dac03cd0f3c46e8aa43af267d23718040bfeb42a
Durran Wallace <Durrantheman@yahoo.com>
2019-09-07 18:44:37 -0500

    Complete repeat string exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var newString = '';
+    if (num < 0) {
+        return 'ERROR';
+    } else if (num == 0) {
+        return '';
+    } else {
+        for (i = 1; i <= num; i++) {
+            newString += string;
+        }
+    }
+    return newString;
 }
 
 module.exports = repeatString
