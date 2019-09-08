cfb6370011af4b43b4257a571957f4abd4799ef6
Durran Wallace <Durrantheman@yahoo.com>
2019-09-07 20:38:39 -0500

    Complete remove from array exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    var arr = array;
+    var arg = Array.from(arguments);
+    for(i = 1; i < arg.length; i++) {
+        if (arr.indexOf(arg[i]) == -1) {
+            continue;
+        } else {
+            arr.splice(arr.indexOf(arg[i]), 1);
+        }    
+    }
 
+    return arr;
 }
 
 module.exports = removeFromArray
