eace30d0990d86b1a9da1283a67866c3ca5bd3d4
Durran Wallace <Durrantheman@yahoo.com>
2019-09-08 21:31:18 -0500

    Complete tempConversion Exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  var C = (temp - 32) * (5/9);
+  if (C % 1 == 0){
+    return Number(C);
+  } else {
+  return Number(C.toFixed(1));
+  }
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  var F = temp * (9/5) + 32;
+  if (F % 1 == 0) {
+    return Number(F);
+  } else{
+  return Number(F.toFixed(1));
+  }
 }
 
 module.exports = {
